0fdb921931e9d3c39129d8076af611ccf6f4c4a9
Tamas Szoke <tamas.szoke.dev@gmail.com>
2018-05-24 20:32:33 +0200

    [TESTS]: Add functions

--- test/test.js
+++ test/test.js
@@ -4,6 +4,38 @@ const expect = require('chai').expect;
 const log = require('../index');
 const text = 'Example text';
 
+const palette = {
+	effect: {
+		reset: "\x1b[0m",
+		bright: "\x1b[1m",
+		dim: "\x1b[2m",
+		underscore: "\x1b[4m",
+		blink: "\x1b[5m",
+		reverse: "\x1b[7m",
+		hidden: "\x1b[8m"
+	},
+	font: {
+		black: "\x1b[30m",
+		red: "\x1b[31m",
+		green: "\x1b[32m",
+		yellow: "\x1b[33m",
+		blue: "\x1b[34m",
+		magenta: "\x1b[35m",
+		cyan: "\x1b[36m",
+		white: "\x1b[37m"
+	},
+	background: {
+		black: "\x1b[40m",
+		red: "\x1b[41m",
+		green: "\x1b[42m",
+		yellow: "\x1b[43m",
+		blue: "\x1b[44m",
+		magenta: "\x1b[45m",
+		cyan: "\x1b[46m",
+		white: "\x1b[47m"
+	}
+};
+
 describe('#eyecatcher', function() {
 
 	it('should get 0 from log', function() {
@@ -83,50 +115,18 @@ describe('#eyecatcher', function() {
 
 	it('should get source row', function() {
 
-		const result = 'test.js:88:17';
+		const result = 'test.js';
 
 		const trace = new Error().stack;
 		let source = trace.split('at')[2].split(' ');		
 		source = source[0].replace(/^.*[\\\/]/, '').replace(/[{()}]/g, '').replace(/\n|\r/g, "");
 
-		expect(result).to.equal(source);
+		expect(source).to.include(result);
 	});
 
 	it('should get colors', function() {
 
 		const result = log._getColors('info');
-
-		const palette = {
-			effect: {
-				reset: "\x1b[0m",
-				bright: "\x1b[1m",
-				dim: "\x1b[2m",
-				underscore: "\x1b[4m",
-				blink: "\x1b[5m",
-				reverse: "\x1b[7m",
-				hidden: "\x1b[8m"
-			},
-			font: {
-				black: "\x1b[30m",
-				red: "\x1b[31m",
-				green: "\x1b[32m",
-				yellow: "\x1b[33m",
-				blue: "\x1b[34m",
-				magenta: "\x1b[35m",
-				cyan: "\x1b[36m",
-				white: "\x1b[37m"
-			},
-			background: {
-				black: "\x1b[40m",
-				red: "\x1b[41m",
-				green: "\x1b[42m",
-				yellow: "\x1b[43m",
-				blue: "\x1b[44m",
-				magenta: "\x1b[45m",
-				cyan: "\x1b[46m",
-				white: "\x1b[47m"
-			}
-		};
 
 		const colors = {
 			time: palette.font['blue'],
@@ -136,5 +136,35 @@ describe('#eyecatcher', function() {
 		};
 
 		expect(result).to.deep.equal(colors);
+	});
+
+	it('should return string from _splitContent', function() {
+
+		const colors = {
+			background: palette.background['white'],
+			text: palette.font['black'],
+			time: palette.font['white'],
+			source: palette.font['white'] + '>' + palette.font['white'],
+			reset: palette.effect['reset']
+		};
+
+		const result = log._splitContent('text', colors, 5, 5);
+
+		expect(result).to.be.a('string');
+	});
+
+	it('should return object from _getBlock', function() {
+
+		const colors = {
+			background: palette.background['white'],
+			text: palette.font['black'],
+			time: palette.font['white'],
+			source: palette.font['white'] + '>' + palette.font['white'],
+			reset: palette.effect['reset']
+		};
+
+		const result = log._getBlock('text', 'log', colors);
+
+		expect(result).to.be.an('object');
 	});
 });
